e3eb0692bd2a234df963ff74d1644429ec2c8539
ShyShouko <denis1195@gmail.com>
2024-05-12 23:34:03 +0300

    edit README.md, add solution for Calculator.java and CalculatorTest.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,21 @@
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+    public static int add(int a, int b){
+      return  a + b;
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+    public static int dif(int a, int b){
+        return a - b;
     }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
+    public static int div(int a, int b){
+        if (b == 0) {
+            System.out.println("Zero in div");
+            return 1;
+        }
+        return a/b;
     }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
+    public static int times(int a, int b) {
+        return a*b;
     }
-    public int solver(){
-        //TODO inser your realisation in method solver
+    public static int solver(int a, int b){
+        return add(a, b)/2;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,26 +6,26 @@ class CalculatorTest {
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(4, Calculator.add(2, 2));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(4, Calculator.dif(6, 2));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertEquals(1, Calculator.div(8, 0));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(4, Calculator.times(-2, -2));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assertEquals(4,Calculator.solver(4, 4));
     }
 }
