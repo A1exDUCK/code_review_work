9543de92750a969bba487a46e12019906fb3a965
ShyShouko <denis1195@gmail.com>
2024-05-26 19:57:23 +0300

    Upgrade main

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,50 +1,21 @@
 public class Calculator {
-    public static int addAD(int a, int b){
-        return a+b;}
-    public static int addSH(int a, int b){
-        return  a + b;}
-
-    public static int difAD(int a, int b){
-        return a-b;}
-    public static int difSH(int a, int b){
-        return a - b;}
-
-    public static int divAD(int a, int b){
-        return a/b;}
-    public static int divSH(int a, int b){
+    public static int add(int a, int b){
+        return  a + b;
+    }
+    public static int dif(int a, int b){
+        return a - b;
+    }
+    public static int div(int a, int b){
         if (b == 0) {
             System.out.println("Zero in div");
-            return 1;}
-        return a/b;}
-
-    public static int timesAD(int a, int b){
-        return a*b;}
-    public static int timesSH(int a, int b) {
-        return a*b;}
-
-    public static int solverAD(int a,int b){
-        return addAD(a,b)/2;}
-    public static int solverSH(int a, int b){
-        return addSH(a, b)/2;}
-    public int addnamek1305(int a, int b){
-        return a+b;
-    }
-    public int difnamek1305(int a, int b){
-        return a-b;
-    }
-    public int divnamek1305(int a, int b){
-        if (b==0) {
-
-            System.out.println("DIVISION BY 0 MISTAKE");
-            return 0;
+            return 1;
         }
         return a/b;
     }
-    public int timesnamek1305(int a, int b){
-
+    public static int times(int a, int b) {
         return a*b;
     }
-    public int solvernamek1305(int a, int b){
-        return difnamek1305(a, b)/2;
+    public static int solver(int a, int b){
+        return add(a, b)/2;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,60 +1,31 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
-    Calculator Calculator = new Calculator();
 
     @Test
-    void addAD() {
-        assertEquals(4, Calculator.addAD(2, 2));}
-    @Test
-    void addSH() {
-        assertEquals(4, Calculator.addSH(2, 2));}
-    @Test
-    void additional_addnamek1305() {
-        assertEquals(10, Calculator.addnamek1305(5, 5));}
-
-
-    @Test
-    void difAD() {
-        assertEquals(4, Calculator.difAD(6, 2));}
-    @Test
-    void difSH() {
-        assertEquals(4, Calculator.difSH(6, 2));}
-    @Test
-    void additional_subtractnamek1305() {
-        assertEquals(2, Calculator.difnamek1305(8, 6));}
-
-
-    @Test
-    void divAD() {
-        assertEquals(5, Calculator.divAD(10, 2));}
-    @Test
-    void divSH() {
-        assertEquals(1, Calculator.divSH(8, 0));}
-    @Test
-    void additional_divisionnamek1305() {
-        assertEquals(0, Calculator.divnamek1305(16, 0));}
-
-
-    @Test
-    void timesAD() {
-assertEquals(20, Calculator.timesAD(10, 2));}
-    @Test
-    void timesSH() {
-        assertEquals(4, Calculator.timesSH(-2, -2));}
-    @Test
-    void additional_multiplicationnamek1305() {
-        assertEquals(3, Calculator.timesnamek1305(-3, -1));
+    void add() {
+        assertEquals(4, Calculator.add(2, 2));
     }
 
+    @Test
+    void dif() {
+        assertEquals(4, Calculator.dif(6, 2));
+    }
 
     @Test
-    void solverAD() {
-        assertEquals(6, Calculator.solverAD(10, 2));}
+    void div() {
+        assertEquals(1, Calculator.div(8, 0));
+    }
+
     @Test
-    void solverSH() {
-        assertEquals(4,Calculator.solverSH(4, 4));}
+    void times() {
+        assertEquals(4, Calculator.times(-2, -2));
+    }
+
     @Test
-    void additional_solvernamek1305() {
-        assertEquals(4, Calculator.solvernamek1305(10, 2));}
+    void solver() {
+        assertEquals(4,Calculator.solver(4, 4));
+    }
 }
